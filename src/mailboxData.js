b8bc5d2251c560dab2f74dc12a09c26db518f5bf
rupesh-brs <alajangirupesh@gmail.com>
2024-11-06 09:26:18 +0530

    adding

---
+++ src/mailboxData.js
@@ -0,0 +1,11 @@
+// src/mailboxData.js
+const mailbox = [
+    { id: 1, sender: 'Ram', subject: 'Payment Issue', date: 'May 29', read: false },
+    { id: 2, sender: 'Raghav', subject: 'Job related', date: 'Jun 15', read: false },
+    { id: 3, sender: 'Raju', subject: 'Loan Approval', date: 'Aug 23', read: false },
+    { id: 4, sender: 'Rudhra', subject: 'Bank Statement', date: 'Aug 29', read: false },
+    { id: 3, sender: 'Rana D', subject: 'Issurance Claim', date: 'Sep 23', read: false }
+    
+];
+
+module.exports = mailbox;
